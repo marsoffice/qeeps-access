081c0eb6a3817a16bd1e80d0a3810f1b3e3767db
Alin Florin <alin@qoffice.ro>
2021-08-03 22:09:14 +0300

    =

--- MarsOffice.Qeeps.Access/GetAllGroups.cs
+++ MarsOffice.Qeeps.Access/GetAllGroups.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 
 namespace MarsOffice.Qeeps.Access
@@ -20,8 +19,7 @@ namespace MarsOffice.Qeeps.Access
 
         [FunctionName("MyGroups")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
         {
             var principal = QeepsPrincipal.Parse(req);
             var groupIds = principal.FindAll(x => x.Type == "groups").Select(x => x.Value).Distinct().ToList();
