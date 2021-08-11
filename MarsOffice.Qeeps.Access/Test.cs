bea0a49983f98081f5d0fdd34b36842880870c3f
Alin Florin <alin@qoffice.ro>
2021-08-12 00:27:24 +0300

    ffg

---
+++ MarsOffice.Qeeps.Access/Test.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using MarsOffice.Qeeps.Access.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace MarsOffice.Qeeps.Access
+{
+    public class Test
+    {
+        public Test()
+        {
+        }
+
+        [FunctionName("Test")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "test")] HttpRequest req)
+        {
+            
+            return new OkObjectResult(new OrganisationDto {
+                Id = "1",
+                Name = "test"
+            });
+        }
+    }
+}
