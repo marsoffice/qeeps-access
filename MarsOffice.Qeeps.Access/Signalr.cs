c5640ba3e8caf41067bbea36a3b08816a9e709b9
Alin Florin <alin@qoffice.ro>
2021-08-18 20:00:12 +0300

    r

--- MarsOffice.Qeeps.Access/Signalr.cs
+++ MarsOffice.Qeeps.Access/Signalr.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using MarsOffice.Qeeps.Access.Abstractions;
-using MarsOffice.Qeeps.Microfunction;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
@@ -21,16 +15,10 @@ namespace MarsOffice.Qeeps.Access
         [FunctionName("SignalrNegotiate")]
         public async Task<SignalRConnectionInfo> SignalrNegotiate(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "api/access/signalr/negotiate")] HttpRequest req,
-            IBinder binder
+            [SignalRConnectionInfo(HubName = "main", UserId = "{headers.x-ms-client-principal-id}")] SignalRConnectionInfo connectionInfo
             )
         {
-            var principal = QeepsPrincipal.Parse(req);
-            var connectionInfo = await binder.BindAsync<SignalRConnectionInfo>(new SignalRConnectionInfoAttribute
-            {
-                HubName = "main",
-                UserId = principal.FindFirstValue("id"),
-                ConnectionStringSetting = "signalrconnectionstring"
-            });
+            await Task.CompletedTask;
             return connectionInfo;
         }
     }
