ecf6a62c745c82198614e578f324b7cb7f70a972
Alin Florin <alin@qoffice.ro>
2021-08-21 12:26:35 +0300

    d

---
+++ MarsOffice.Qeeps.Access/Users.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using MarsOffice.Qeeps.Access.Abstractions;
+using MarsOffice.Qeeps.Access.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Newtonsoft.Json;
+
+namespace MarsOffice.Qeeps.Access
+{
+    public class Users
+    {
+        private readonly IMapper _mapper;
+        public Users(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [FunctionName("GetUsers")]
+        public async Task<IActionResult> GetUsers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "api/access/users")] HttpRequest req,
+            ClaimsPrincipal principal,
+            [CosmosDB(
+                ConnectionStringSetting = "cdbconnectionstring")] DocumentClient client
+            )
+        {
+#if DEBUG
+            var db = new Database
+            {
+                Id = "access"
+            };
+            await client.CreateDatabaseIfNotExistsAsync(db);
+
+
+            var col = new DocumentCollection
+            {
+                Id = "Users",
+                PartitionKey = new PartitionKeyDefinition
+                {
+                    Version = PartitionKeyDefinitionVersion.V1,
+                    Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                }
+            };
+            await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), col);
+#endif
+
+            var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+            if (env != "Development" && principal.FindFirstValue("roles") != "Application")
+            {
+                return new StatusCodeResult(401);
+            }
+
+            using var streamReader = new StreamReader(req.Body);
+            var json = await streamReader.ReadToEndAsync();
+            var ids = JsonConvert.DeserializeObject<IEnumerable<string>>(json);
+            var colUri = UriFactory.CreateDocumentCollectionUri("access", "Users");
+            var query = client.CreateDocumentQuery(colUri, new FeedOptions
+            {
+                PartitionKey = new PartitionKey("UserEntity")
+            })
+            .Where(x => ids.Contains(x.Id))
+            .AsDocumentQuery();
+
+            var userDtos = new List<UserDto>();
+            while (query.HasMoreResults)
+            {
+                var userEntities = await query.ExecuteNextAsync<UserEntity>();
+                userDtos.AddRange(
+                    _mapper.Map<IEnumerable<UserDto>>(userEntities)
+                );
+            }
+
+            return new OkObjectResult(userDtos);
+        }
+
+        [FunctionName("GetUser")]
+        public async Task<IActionResult> GetUser(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/access/user/{id}")] HttpRequest req,
+            ClaimsPrincipal principal,
+            [CosmosDB(
+                ConnectionStringSetting = "cdbconnectionstring")] DocumentClient client
+            )
+        {
+#if DEBUG
+            var db = new Database
+            {
+                Id = "access"
+            };
+            await client.CreateDatabaseIfNotExistsAsync(db);
+
+
+            var col = new DocumentCollection
+            {
+                Id = "Users",
+                PartitionKey = new PartitionKeyDefinition
+                {
+                    Version = PartitionKeyDefinitionVersion.V1,
+                    Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                }
+            };
+            await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), col);
+#endif
+
+            var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+            if (env != "Development" && principal.FindFirstValue("roles") != "Application")
+            {
+                return new StatusCodeResult(401);
+            }
+            var id = req.RouteValues["id"].ToString();
+            UserEntity found = null;
+            try
+            {
+                var uri = UriFactory.CreateDocumentUri("access", "Users", id);
+                found = (
+                    await client.ReadDocumentAsync<UserEntity>(uri, new RequestOptions {
+                        PartitionKey = new PartitionKey("UserEntity")
+                    })
+                )?.Document;
+            }
+            catch (Exception) { }
+
+            if (found == null)
+            {
+                return new StatusCodeResult(404);
+            }
+
+            return new OkObjectResult(
+                _mapper.Map<UserDto>(found)
+            );
+        }
+    }
+}
