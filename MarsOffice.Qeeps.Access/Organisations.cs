f6b01b795a3af313ada9a3dcdd32b113edaf3ea8
Alin Florin <alin@qoffice.ro>
2021-12-01 23:14:40 +0200

    f

--- MarsOffice.Qeeps.Access/Organisations.cs
+++ MarsOffice.Qeeps.Access/Organisations.cs
@@ -197,7 +197,7 @@ namespace MarsOffice.Qeeps.Access
 
 
         [FunctionName("GetFullOrganisationsTreeForUser")]
-        public async Task<IActionResult> GetMyFullOrganisationsTreeForUser(
+        public async Task<IActionResult> GetFullOrganisationsTreeForUser(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/access/getFullOrganisationsTree/{userId}")] HttpRequest req,
             [CosmosDB(ConnectionStringSetting = "cdbconnectionstring", PreferredLocations = "%location%")] DocumentClient client,
             ILogger log,
@@ -270,6 +270,97 @@ namespace MarsOffice.Qeeps.Access
                         PartitionKey = new PartitionKey("OrganisationEntity")
                     })
                     .Where(x => x.FullId.Contains("_" + orgId))
+                    .AsDocumentQuery();
+
+                    while (foundOrgsQuery.HasMoreResults)
+                    {
+                        dtos.AddRange(_mapper.Map<IEnumerable<OrganisationDto>>(await foundOrgsQuery.ExecuteNextAsync<OrganisationEntity>()));
+                    }
+                }
+                dtos = dtos.DistinctBy(x => x.FullId).ToList();
+                return new OkObjectResult(dtos);
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Exception occured in function");
+                return new BadRequestObjectResult(Errors.Extract(e));
+            }
+        }
+
+        [FunctionName("GetFullOrganisationsTreeForUser")]
+        public async Task<IActionResult> GetAccessibleOrganisationsForUser(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/access/getAccessibleOrganisations/{userId}")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "cdbconnectionstring", PreferredLocations = "%location%")] DocumentClient client,
+            ILogger log,
+            ClaimsPrincipal principal
+            )
+        {
+            try
+            {
+#if DEBUG
+                var db = new Database
+                {
+                    Id = "access"
+                };
+                await client.CreateDatabaseIfNotExistsAsync(db);
+
+                var col = new DocumentCollection
+                {
+                    Id = "Organisations",
+                    PartitionKey = new PartitionKeyDefinition
+                    {
+                        Version = PartitionKeyDefinitionVersion.V2,
+                        Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                    }
+                };
+                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), col);
+
+                col = new DocumentCollection
+                {
+                    Id = "OrganisationAccesses",
+                    PartitionKey = new PartitionKeyDefinition
+                    {
+                        Version = PartitionKeyDefinitionVersion.V2,
+                        Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                    }
+                };
+                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), col);
+#endif
+                client.ConnectionPolicy.UseMultipleWriteLocations = _config.GetValue<bool>("multimasterdatabase");
+
+                var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+                if (env != "Development" && principal.FindFirstValue("roles") != "Application")
+                {
+                    return new StatusCodeResult(401);
+                }
+                var uid = req.RouteValues["userId"].ToString();
+
+                var orgAccessesCollection = UriFactory.CreateDocumentCollectionUri("access", "OrganisationAccesses");
+                var foundAccessesQuery = client.CreateDocumentQuery<OrganisationAccessEntity>(orgAccessesCollection, new FeedOptions
+                {
+                    PartitionKey = new PartitionKey("OrganisationAccessEntity")
+                })
+                .Where(x => x.UserId == uid)
+                .AsDocumentQuery();
+
+                var entities = new List<OrganisationAccessEntity>();
+                while (foundAccessesQuery.HasMoreResults)
+                {
+                    entities.AddRange(await foundAccessesQuery.ExecuteNextAsync<OrganisationAccessEntity>());
+                }
+
+
+                var dtos = new List<OrganisationDto>();
+                var orgIds = entities.Select(x => x.OrganisationId).Distinct().ToList();
+                var orgsCollection = UriFactory.CreateDocumentCollectionUri("access", "Organisations");
+
+                foreach (var orgId in orgIds)
+                {
+                    var foundOrgsQuery = client.CreateDocumentQuery<OrganisationEntity>(orgsCollection, new FeedOptions
+                    {
+                        PartitionKey = new PartitionKey("OrganisationEntity")
+                    })
+                    .Where(x => x.Id == orgId)
                     .AsDocumentQuery();
 
                     while (foundOrgsQuery.HasMoreResults)
