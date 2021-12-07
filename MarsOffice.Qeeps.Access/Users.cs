ff7f318f49bd30d0055af9c92b9ddb5aa10d3780
Alin Florin <alin@qoffice.ro>
2021-12-07 13:20:32 +0200

    -

--- MarsOffice.Qeeps.Access/Users.cs
+++ MarsOffice.Qeeps.Access/Users.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -305,6 +306,166 @@ namespace MarsOffice.Qeeps.Access
                 }, true);
 
                 return new OkResult();
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Exception occured in function");
+                return new BadRequestObjectResult(Errors.Extract(e));
+            }
+        }
+
+        [FunctionName("GetUsersByOrganisationId")]
+        public async Task<IActionResult> GetUserByOrganisationId(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/access/user/{organisationId}")] HttpRequest req,
+            ClaimsPrincipal principal,
+            [CosmosDB(
+                ConnectionStringSetting = "cdbconnectionstring", PreferredLocations = "%location%")] DocumentClient client,
+            ILogger log
+            )
+        {
+            try
+            {
+                client.ConnectionPolicy.UseMultipleWriteLocations = _config.GetValue<bool>("multimasterdatabase");
+#if DEBUG
+                var db = new Database
+                {
+                    Id = "access"
+                };
+                await client.CreateDatabaseIfNotExistsAsync(db);
+
+
+                var col = new DocumentCollection
+                {
+                    Id = "Users",
+                    PartitionKey = new PartitionKeyDefinition
+                    {
+                        Version = PartitionKeyDefinitionVersion.V2,
+                        Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                    }
+                };
+                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), col);
+
+                var accessesCol = new DocumentCollection
+                {
+                    Id = "OrganisationAccesses",
+                    PartitionKey = new PartitionKeyDefinition
+                    {
+                        Version = PartitionKeyDefinitionVersion.V2,
+                        Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                    }
+                };
+                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), accessesCol);
+
+                var orgsCol = new DocumentCollection
+                {
+                    Id = "Organisations",
+                    PartitionKey = new PartitionKeyDefinition
+                    {
+                        Version = PartitionKeyDefinitionVersion.V2,
+                        Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                    }
+                };
+                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), orgsCol);
+#endif
+
+                var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+                if (env != "Development" && principal.FindFirstValue("roles") != "Application")
+                {
+                    return new StatusCodeResult(401);
+                }
+                var organisationId = req.RouteValues["organisationId"].ToString();
+                var includeDetails = req.Query.ContainsKey("includeDetails") &&
+                     bool.Parse(req.Query["includeDetails"].ToString().ToLower());
+
+                var orgsCollection = UriFactory.CreateDocumentCollectionUri("access", "Organisations");
+                var orgAccessesCollection = UriFactory.CreateDocumentCollectionUri("access", "OrganisationAccesses");
+                var usersCollection = UriFactory.CreateDocumentCollectionUri("access", "Users");
+
+                // get org children
+                var orgsQuery = client.CreateDocumentQuery<OrganisationEntity>(orgsCollection, new FeedOptions
+                {
+                    PartitionKey = new PartitionKey("OrganisationEntity")
+                }).Where(x => x.FullId.Contains("_" + organisationId))
+                .Select(x => x.Id)
+                .Distinct()
+                .AsDocumentQuery();
+
+                var orgIds = new List<string>();
+                while (orgsQuery.HasMoreResults)
+                {
+                    var orgIdsResponse = await orgsQuery.ExecuteNextAsync<string>();
+                    orgIds.AddRange(orgIdsResponse);
+                }
+
+                if (!orgIds.Any())
+                {
+                    return new OkObjectResult(new List<UserDto>());
+                }
+
+                var userIdsQuery = client.CreateDocumentQuery<OrganisationAccessEntity>(
+                    orgAccessesCollection, new FeedOptions
+                    {
+                        PartitionKey = new PartitionKey("OrganisationAccessEntity")
+                    }
+                ).Where(x => orgIds.Contains(x.OrganisationId))
+                .Select(x => x.UserId)
+                .Distinct()
+                .AsDocumentQuery();
+
+                var userIds = new List<string>();
+                while (userIdsQuery.HasMoreResults)
+                {
+                    var userIdsResponse = await userIdsQuery.ExecuteNextAsync<string>();
+                    userIds.AddRange(userIdsResponse);
+                }
+
+                if (!userIds.Any())
+                {
+                    return new OkObjectResult(new List<UserDto>());
+                }
+
+                int batchSize = 1000;
+                var noOfBatches = (int)Math.Ceiling(userIds.Count * 1f / batchSize);
+                var usersQuery = client.CreateDocumentQuery<UserEntity>(usersCollection, new FeedOptions
+                {
+                    PartitionKey = new PartitionKey("UserEntity")
+                });
+
+                Expression<Func<UserEntity, UserEntity>> selectExpression;
+
+                if (includeDetails)
+                {
+                    selectExpression = x => new UserEntity
+                    {
+                        Id = x.Id,
+                        Email = x.Email,
+                        UserPreferences = x.UserPreferences
+                    };
+                }
+                else
+                {
+                    selectExpression = x => new UserEntity
+                    {
+                        Id = x.Id
+                    };
+                }
+
+                var userDtos = new List<UserDto>();
+
+                for (var i = 0; i < noOfBatches; i++)
+                {
+                    var slice = userIds.Skip((int)i * batchSize).Take(batchSize).ToList();
+                    var docQuery = usersQuery.Where(x => slice.Contains(x.Id))
+                        .Select(selectExpression)
+                        .AsDocumentQuery();
+                    while (docQuery.HasMoreResults)
+                    {
+                        var response = await docQuery.ExecuteNextAsync<UserEntity>();
+                        userDtos.AddRange(_mapper.Map<IEnumerable<UserDto>>(response));
+                    }
+                }
+
+                return new OkObjectResult(userDtos);
             }
             catch (Exception e)
             {
