72019f1749efe93149084077ed956640efc7a0db
Alin Florin <alin@qoffice.ro>
2022-01-09 17:19:13 +0200

    d

--- MarsOffice.Qeeps.Access/UserPreferences.cs
+++ MarsOffice.Qeeps.Access/UserPreferences.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using MarsOffice.Microfunction;
@@ -10,6 +12,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
@@ -158,6 +161,76 @@ namespace MarsOffice.Qeeps.Access
                 }, true);
 
                 return new OkResult();
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Exception occured in function");
+                return new BadRequestObjectResult(Errors.Extract(e));
+            }
+        }
+
+        [FunctionName("GetUserPreferencesByUserIds")]
+        public async Task<IActionResult> GetUserPreferencesByUserIds(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "api/access/userPreferencesByUserIds")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "cdbconnectionstring", PreferredLocations = "%location%")] DocumentClient client,
+            ILogger log,
+            ClaimsPrincipal principal
+            )
+        {
+            try
+            {
+                client.ConnectionPolicy.UseMultipleWriteLocations = _config.GetValue<bool>("multimasterdatabase");
+#if DEBUG
+                var db = new Database
+                {
+                    Id = "access"
+                };
+                await client.CreateDatabaseIfNotExistsAsync(db);
+
+                var col = new DocumentCollection
+                {
+                    Id = "UserPreferences",
+                    PartitionKey = new PartitionKeyDefinition
+                    {
+                        Version = PartitionKeyDefinitionVersion.V2,
+                        Paths = new System.Collections.ObjectModel.Collection<string>(new List<string>() { "/Partition" })
+                    }
+                };
+                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("access"), col);
+#endif
+
+                var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+                if (env != "Development" && principal.FindFirstValue("roles") != "Application")
+                {
+                    return new StatusCodeResult(401);
+                }
+
+                var json = string.Empty;
+                using (var streamReader = new StreamReader(req.Body))
+                {
+                    json = await streamReader.ReadToEndAsync();
+                }
+                var payload = JsonConvert.DeserializeObject<IEnumerable<string>>(json, new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+
+                var userPrefsCol = UriFactory.CreateDocumentCollectionUri("access", "userPreferences");
+                var query = client.CreateDocumentQuery<UserPreferencesEntity>(userPrefsCol, new FeedOptions
+                {
+                    PartitionKey = new PartitionKey("UserPreferencesEntity")
+                })
+                .Where(x => payload.Contains(x.Id))
+                .AsDocumentQuery();
+
+                var dtos = new List<UserPreferencesDto>();
+                while (query.HasMoreResults) {
+                    var r = await query.ExecuteNextAsync<UserPreferencesEntity>();
+                    dtos.AddRange(
+                        _mapper.Map<IEnumerable<UserPreferencesDto>>(r)
+                    );
+                }
+                return new OkObjectResult(dtos);
             }
             catch (Exception e)
             {
